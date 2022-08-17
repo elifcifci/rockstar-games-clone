5166cbd31720261ec901c9479cc24f6bf9e47a52
Elif Çiftçi <eliffbat@gmail.com>
2022-08-17 23:53:30 +0300

    created global styles

--- src/Styles/globalStyles.js
+++ src/Styles/globalStyles.js
@@ -1,1 +1,21 @@
 import { createGlobalStyle } from "styled-components";
+
+export const colors = {
+  primary: "#0b0b0b",
+  secondary: "#fcaf17",
+  tertiary: "#e8e8e8",
+  navbarBackground: "rgba(0,0,0,.85)",
+  border: "#282828",
+  cardBackground: "#000",
+  text: "#fff",
+};
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        background-color: ${colors.primary};
+        color: ${colors.text};
+        font-family: helvetica;
+    }
+`;
+
+export default GlobalStyle;
