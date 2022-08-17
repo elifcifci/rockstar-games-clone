e9447d54acd2b6375344f4b5789d51dc6c680e1a
Elif Çiftçi <eliffbat@gmail.com>
2022-08-17 18:19:17 +0300

    constants created

---
+++ src/Constants/photographsLink.js
@@ -0,0 +1,20 @@
+export const sliderPhotograph = [
+  {
+    title: "The Criminal Enterprises Now Available",
+    link: "https://videos-rockstargames-com.akamaized.net/screencaps/12263/_global/1920.jpg",
+  },
+  {
+    title: "The New Declasse Draugur Out Now",
+    link: "https://videos-rockstargames-com.akamaized.net/screencaps/12266/_global/1920.jpg",
+  },
+  {
+    title: "The Criminal Enterprises, Coming July 26",
+    link: "https://videos-rockstargames-com.akamaized.net/screencaps/12261/_global/1920.jpg",
+  },
+  {
+    title: "Out Now on PlayStation 5 and Xbox Series X|S",
+    link: "https://videos-rockstargames-com.akamaized.net/screencaps/12258/_global/1920.jpg",
+  },
+];
+
+
