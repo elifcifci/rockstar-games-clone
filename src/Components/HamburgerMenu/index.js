4383673ddce2baf8e4746e9bdc9638f7e381ba89
Elif Çiftçi <eliffbat@gmail.com>
2022-09-01 00:02:51 +0300

    the active page name set to be displayed on the header.

--- src/Components/HamburgerMenu/index.js
+++ src/Components/HamburgerMenu/index.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+
 import MenuItem from "../MenuItem";
+import { menuItems } from "../../Constants/Navbar";
 import { MenuToggleIcon } from "../MenuToggle";
+
 import { Container } from "./styles";
 import { useCycle } from "framer-motion";
 
 export default function HamburgerMenu({ click }) {
   const [isOpen, toggleOpen] = useCycle(false, true);
-
+  let pageName = useLocation().pathname;
   return (
     <Container
       onClick={click}
@@ -15,6 +19,7 @@ export default function HamburgerMenu({ click }) {
     >
       <MenuItem />
       <MenuToggleIcon toggle={() => toggleOpen()} />
+      {menuItems.map((item) => item.link === pageName && item.title)}
     </Container>
   );
 }
--- src/Components/HamburgerMenu/styles.js
+++ src/Components/HamburgerMenu/styles.js
@@ -5,7 +5,10 @@ export const Container = styled(motion.div)`
   position: relative;
   top: 0;
   z-index: 3;
-
+  display: flex;
+  grid-gap: 1rem;
+  align-items: center;
+  
   .background {
     width: 300px;
     background: black;
