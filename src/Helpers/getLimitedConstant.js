5feeb7b5672bcb11a30aa76fbcdeaf0d2af88d01
Elif Çiftçi <eliffbat@gmail.com>
2022-10-22 09:48:51 +0300

    fix for netlify

--- src/Helpers/getLimitedConstant.js
+++
@@ -1,8 +0,0 @@
-export function getLimitedConstant(requestedLimit, [...constant]) {
-  const controlledConstant = [];
-
-  for (let index = 0; index <= requestedLimit; index++) {
-    constant[index] !== undefined && controlledConstant.push(constant[index]);
-  }
-  return controlledConstant;
-}
--- src/Pages/Newswire/styles.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const StyledNewswireContainer = styled.div`
-  margin-bottom: 2rem;
-`;
